05a38342882728079f0b9c923ab588e8277b2c25
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-08-15 17:36:45 -0400

    August 15, 2015 Commit 1

---
+++ src/binarytree/BinaryTree.java
@@ -0,0 +1,42 @@
+package binarytree;
+
+import java.util.*;
+
+/*
+* Julian Saavedra
+* github.com/jfsaaved
+* julian.felipe.saavedra@gmail.com
+*/
+
+public class BinaryTree<AnyType extends Comparable<AnyType>>{
+	
+	private Node<AnyType> parent;
+
+	public BinaryTree(){
+		parent = null;
+	}
+	
+	public void insertNode(AnyType data){
+		Node<AnyType> newNode = new Node<AnyType>(data);
+	
+		if(parent == null){
+			parent = newNode;
+		}
+		else{
+			Node<AnyType> tmp = parent;
+			while(tmp.getLeft() != null){
+				tmp = tmp.getLeft();
+			}
+			tmp.setLeft(newNode);
+		}
+	}
+	
+	public AnyType getParentData(){
+		return parent.getData();
+	}
+	
+	public Node<AnyType> getParent(){
+		return parent;
+	}
+
+}
---
+++ src/binarytree/Node.java
@@ -0,0 +1,43 @@
+package binarytree;
+
+import java.util.*;
+
+/*
+* Julian Saavedra
+* github.com/jfsaaved
+* julian.felipe.saavedra@gmail.com
+*/
+
+public class Node<AnyType>{
+
+	private AnyType data;
+	private Node<AnyType> left;
+	private Node<AnyType> right;
+
+	public Node(AnyType data){
+		this.data = data;
+		left = null;
+		right = null;
+	}
+	
+	public void setLeft(Node<AnyType> left){
+		this.left = left;
+	}
+	
+	public void setRigth(Node<AnyType> right){
+		this.right = right;
+	}
+	
+	public Node<AnyType> getLeft(){
+		return left;
+	}
+
+	public Node<AnyType> getRight(){
+		return right;
+	}
+	
+	public AnyType getData(){
+		return data;
+	}
+
+}
